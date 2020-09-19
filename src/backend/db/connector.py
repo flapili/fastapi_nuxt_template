dbd388f31243ecb532e89a6a8a645cd3894ef2bf
Deveaux Benoît <contact.flapili@gmail.com>
2020-09-19 17:01:36 +0200

    use depends instead of import db object

--- src/backend/db/connector.py
+++ src/backend/db/connector.py
@@ -1,5 +1,10 @@
+from functools import lru_cache
+
 from databases import Database
 
 from backend.core.config import config
 
-db = Database(config.PSQL_URL)
+
+@lru_cache
+async def get_db():
+    return Database(config.PSQL_URL)
